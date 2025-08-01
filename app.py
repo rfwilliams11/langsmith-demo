fa7fb618d4466c757d44643834ac1615b4497e76
Reese Williams <rfwilliams11@gmail.com>
2025-07-31 21:12:32 -0700

    Reorganize files

--- app.py
+++ app.py
@@ -1,115 +1,59 @@
 import os
-import tempfile
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders.sitemap import SitemapLoader
-from langchain_community.vectorstores import SKLearnVectorStore
-from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from langchain import hub
 from langsmith import traceable
+from langsmith.client import convert_prompt_to_openai_format
 from openai import OpenAI
+from openai.types.chat import ChatCompletion, ChatCompletionMessageParam
 from typing import List
 import nest_asyncio
+import retriever
+
+# os.environ["OPENAI_API_KEY"] = ""
+# os.environ["LANGCHAIN_API_KEY"] = ""
+# os.environ["LANGSMITH_TRACING_V2"] = "true"
+# os.environ["LANGCHAIN_PROJECT"] = "langsmith-demo-4"
+
+load_dotenv(dotenv_path="../../.env", override=True)
+nest_asyncio.apply()
 
 MODEL_NAME = "gpt-4o-mini"
 MODEL_PROVIDER = "openai"
-APP_VERSION = 1.0
-RAG_SYSTEM_PROMPT = """You are an assistant for question-answering tasks. 
-Use the following pieces of retrieved context to answer the latest question in the conversation. 
-If you don't know the answer, just say that you don't know. 
-Use three sentences maximum and keep the answer concise.
-"""
+
+# RAG_SYSTEM_PROMPT = """You are an assistant for question-answering tasks.
+# Use the following pieces of retrieved context to answer the latest question in the conversation.
+# If you don't know the answer, just say that you don't know.
+# Use one sentence maximum and keep the answer concise.
+# """
+
+# Note that we are pulling our prompt from LangChain's Hub
+prompt = hub.pull("ls-demo-v1")
 
 openai_client = OpenAI()
 
 
-def get_vector_db_retriever():
-    persist_path = os.path.join(tempfile.gettempdir(), "union.parquet")
-    embd = OpenAIEmbeddings()
-
-    # If vector store exists, then load it
-    if os.path.exists(persist_path):
-        vectorstore = SKLearnVectorStore(
-            embedding=embd, persist_path=persist_path, serializer="parquet"
-        )
-        return vectorstore.as_retriever(lambda_mult=0)
-
-    # Otherwise, index LangSmith documents and create new vector store
-    ls_docs_sitemap_loader = SitemapLoader(
-        web_path="https://docs.smith.langchain.com/sitemap.xml",
-        continue_on_failure=True,
-    )
-    ls_docs = ls_docs_sitemap_loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=500, chunk_overlap=0
-    )
-    doc_splits = text_splitter.split_documents(ls_docs)
-
-    vectorstore = SKLearnVectorStore.from_documents(
-        documents=doc_splits,
-        embedding=embd,
-        persist_path=persist_path,
-        serializer="parquet",
-    )
-    vectorstore.persist()
-    return vectorstore.as_retriever(lambda_mult=0)
-
-
-nest_asyncio.apply()
-retriever = get_vector_db_retriever()
-
-"""
-retrieve_documents
-- Returns documents fetched from a vectorstore based on the user's question
-"""
-
-
 @traceable(run_type="chain")
 def retrieve_documents(question: str):
-    return retriever.invoke(question)
-
-
-"""
-generate_response
-- Calls `call_openai` to generate a model response after formatting inputs
-"""
-
-
-@traceable(run_type="chain")
-def generate_response(question: str, documents):
-    formatted_docs = "\n\n".join(doc.page_content for doc in documents)
-    messages = [
-        {"role": "system", "content": RAG_SYSTEM_PROMPT},
-        {
-            "role": "user",
-            "content": f"Context: {formatted_docs} \n\n Question: {question}",
-        },
-    ]
-    return call_openai(messages)
-
-
-"""
-call_openai
-- Returns the chat completion output from OpenAI
-"""
+    return retriever.retriever.invoke(question)
 
 
 @traceable(
     run_type="llm",
     metadata={"ls_provider": MODEL_PROVIDER, "ls_model_name": MODEL_NAME},
 )
-def call_openai(messages: List[dict]) -> str:
+def call_openai(messages: List[ChatCompletionMessageParam]) -> ChatCompletion:
     return openai_client.chat.completions.create(
         model=MODEL_NAME,
         messages=messages,
     )
 
 
-"""
-langsmith_rag
-- Calls `retrieve_documents` to fetch documents
-- Calls `generate_response` to generate a response based on the fetched documents
-- Returns the model response
-"""
+@traceable(run_type="chain")
+def generate_response(question: str, documents):
+    formatted_docs = "\n\n".join(doc.page_content for doc in documents)
+    formatted_prompt = prompt.invoke({"context": formatted_docs, "question": question})
+    messages = convert_prompt_to_openai_format(formatted_prompt)["messages"]
+    return call_openai(messages)
 
 
 @traceable(run_type="chain")
